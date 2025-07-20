d5a8d0ae682dfc2a62b76427770b58863551cf94
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-07-20 19:52:10 +0200

    Delete initial_train.py

--- initial_train.py
+++
@@ -1,6 +0,0 @@
-from auto_retrain import ModelRetrainer, PocketOptionAPI
-
-if __name__ == "__main__":
-    api = PocketOptionAPI()
-    retrainer = ModelRetrainer(api)
-    retrainer.retrain_model()
